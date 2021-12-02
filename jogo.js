0a656759d93566656c42a1a9f55e832ceaa2236f
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-02 20:39:12 -0300

    aplicação game concluido versão 1

--- jogo.js
+++ jogo.js
@@ -1,5 +1,26 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 10
+
+var criaMosquitoTempo = 1700
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+
+    criaMosquitoTempo = 1700
+
+} else if (nivel === 'dificil') {
+
+    criaMosquitoTempo = 1200
+    
+} else if (nivel === 'chucknorris') {
+
+    criaMosquitoTempo = 850
+    
+}
 
 
 function ajustaTamanhoPalcoJogo(){
@@ -11,17 +32,37 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
-function criarBackground() {
-    if(document.getElementsByClassName('.painel')) { 
-        console.log(document.getElementsByClassName('.painel'))
+var cronometro = setInterval(function() {
+
+    tempo -= 1
+
+    if(tempo < 0 ) {
+        clearInterval('cronometro')
+        clearInterval('criaMosca')
+        window.location.href = "vitoria.html"
+    }else {
+
+        document.getElementById('cronometro').innerHTML = tempo
     }
-}
+
+    
+}, 1000)
 
 function posicaoRandomica(){
 
 
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            window.location.href = "fim_de_jogo.html"
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
+            vidas++
+
+        }
+
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -39,6 +80,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function()  {
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
 }
