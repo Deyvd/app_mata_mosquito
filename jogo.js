3eb3e14a69843cbfe1cdbca01344493aadf0ac34
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-02 17:17:27 -0300

    início do projeto

---
+++ jogo.js
@@ -0,0 +1,74 @@
+var altura = 0
+var largura = 0
+
+
+function ajustaTamanhoPalcoJogo(){
+     altura = window.innerHeight
+     largura = window.innerWidth    
+
+    // console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+function criarBackground() {
+    if(document.getElementsByClassName('.painel')) { 
+        console.log(document.getElementsByClassName('.painel'))
+    }
+}
+
+function posicaoRandomica(){
+
+
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosca.png"
+    mosquito.className = tamanhoAleatorio() +" " + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    
+    document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe  = Math.floor(Math.random() * 3)
+    console.log(classe)
+    switch(classe) {
+        case 0: 
+            return 'mosquito1'
+
+        case 1: 
+            return 'mosquito2'
+
+        case 2: 
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe  = Math.floor(Math.random() * 2)
+    console.log(classe)
+    switch(classe) {
+        case 0: 
+            return 'ladoA'
+
+        case 1: 
+            return 'ladoB'
+
+    } 
+}
+
+
